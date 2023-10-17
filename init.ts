948e1a28a3859ec1ec173f2548838a9f9f41e27a
MarryDream <2190758465@qq.com>
2023-10-17 20:27:14 +0800

    feat: automatically pull resource zip after updating resource overruns

--- init.ts
+++ init.ts
@@ -4,6 +4,11 @@ import { definePlugin } from "@/modules/plugin";
 import * as m from "./module";
 import routers from "#/genshin/routes";
 import { getRandomString } from "@/utils/random";
+import { getFileSize } from "@/utils/network";
+import Progress from "@/utils/progress";
+import { formatMemories } from "@/utils/format";
+import compressing from "compressing";
+import { Logger } from "log4js";
 
 const initConfig = {
 	cardWeaponStyle: "normal",
@@ -53,6 +58,69 @@ export default definePlugin( {
 	assets: {
 		manifestUrl: "https://mari-files.oss-cn-beijing.aliyuncs.com/adachi-bot/version3/genshin_assets_manifest.yml",
 		downloadBaseUrl: "https://mari-files.oss-cn-beijing.aliyuncs.com",
+		async overflowHandle( assets, pluginKey, { file, logger } ) {
+			const gLogger = new Proxy( logger, {
+				get( target: Logger, p: string | symbol ): any {
+					return ( content: string ) => target[p]( `${ pluginKey } 插件: ${ content }` );
+				}
+			} );
+			gLogger.info( "待更新资源数量超出限制，开始下载压缩资源包..." );
+			// 超出时下载整包资源
+			const fileUrl = "https://mari-files.oss-cn-beijing.aliyuncs.com/adachi-bot/version3/genshin-resource.zip";
+			const totalSize = await getFileSize( fileUrl );
+			let downloadSize = 0;
+			const progress = new Progress( "下载 genshin 插件整包资源", totalSize || 0 );
+			
+			const startTime = Date.now();
+			
+			// 压缩包下载目标路径
+			const zipDownloadPath: string = "genshin/genshin-resource.zip";
+			try {
+				await file.downloadFileStream( fileUrl, zipDownloadPath, "plugin", chunk => {
+					const curLength = chunk.length;
+					downloadSize += curLength;
+					if ( !totalSize ) {
+						progress.setTotal( downloadSize );
+					}
+					// 下载进度条
+					progress.renderer( downloadSize, () => {
+						if ( totalSize ) {
+							const elapsedTime = ( Date.now() - startTime ) / 1000;
+							const averageSize = downloadSize / elapsedTime;
+							
+							const fDownloadSize = formatMemories( downloadSize, "M" );
+							const fTotalSize = formatMemories( totalSize, "M" );
+							const fAverageSize = formatMemories( averageSize, averageSize < 1024 * 1024 ? "KB" : "M" );
+							return `${ fDownloadSize }/${ fTotalSize } ${ fAverageSize }/s`;
+						}
+						return formatMemories( downloadSize, "M" );
+					} )
+				} );
+			} catch ( error ) {
+				gLogger.error( "资源包下载失败:" + ( <Error>error ).stack );
+				throw error;
+			}
+			// 压缩包解压目标路径
+			const zipUnCompressPath = `${ pluginKey }/${ assets.folderName || "adachi-assets" }`;
+			/* 此时存在原有资源文件，先进行删除 */
+			const { type: originPathFileType } = await file.getFileType( zipUnCompressPath, "plugin" );
+			if ( originPathFileType === "directory" ) {
+				gLogger.info( "正在清除原有资源文件..." );
+				const { status: deleteStatus } = await file.deleteFile( zipUnCompressPath, "plugin" );
+				if ( !deleteStatus ) {
+					gLogger.error( "清除原有资源文件失败，请尝试手动解压替换" );
+					return;
+				}
+			}
+			gLogger.info( "开始解压资源包..." );
+			const { status: unCompressStatus } = await file.unCompressFile( "zip", zipDownloadPath, zipUnCompressPath, "plugin" );
+			if ( !unCompressStatus ) {
+				gLogger.error( "解压资源包失败，请尝试手动解压替换" );
+				return;
+			}
+			gLogger.info( "资源包解压完成" );
+			await file.deleteFile( zipDownloadPath, "plugin" );
+		},
 		replacePath: path => {
 			return path.replace( "adachi-bot/version3/genshin/", "" );
 		}
@@ -93,7 +161,7 @@ export default definePlugin( {
 		}
 	],
 	/* 初始化模块 */
-	mounted( param ) {
+	async mounted( param ) {
 		/* 加载 genshin.yml 配置 */
 		config = param.configRegister( "main", initConfig );
 		const cookieCfg = param.configRegister( "cookies", {
