2c998fcef0f903710ec6111ef2e7363bfd359bc5
MarryDream <2190758465@qq.com>
2023-10-23 02:09:37 +0800

    fix: the led and char instructions do not work

--- views/character/components/score-chart.js
+++ views/character/components/score-chart.js
@@ -1,21 +1,17 @@
 const template = `<div class="score-chart">
-	<v-chart class="chart" :key="chartKey" :option="chartOption"></v-chart>
+	<div class="chart" ref="echartsRef"></div>
 	<p>评分：
 		<span class="score" :style="{ color: color.text }">{{ score }}</span>
 	</p>
 	<p class="desc">*根据等级以及星级评分，仅供娱乐参考</p>
 </div>`
 
-import { defineComponent, ref, onMounted, computed } from "vue";
-import "echarts";
-import VChart from "vue-echarts";
+import { defineComponent, ref, onMounted, computed, watch } from "vue";
+import * as echarts from "echarts";
 
 export default defineComponent( {
 	name: "DataChart",
 	template,
-	components: {
-		VChart
-	},
 	props: {
 		data: {
 			type: Object,
@@ -30,82 +26,94 @@ export default defineComponent( {
 		}
 	},
 	setup( props ) {
-		const chartKey = ref( 0 );
-		
 		const total = 100;
 		
 		const score = computed( () => props.data.total.toFixed( 2 ) )
+
+		const myCharts = ref( null );
+		const echartsRef = ref( null );
 		
-		const chartOption = ref( {
-			animation: false,
-			radar: {
-				shape: "circle",
-				center: [ "50%", "50%" ],
-				radius: "65%",
-				indicator: props.data.list.map( d => {
-					return {
-						name: d.label,
-						max: total
+		function initEcharts() {
+			if ( !myCharts.value ) {
+				return;
+			}
+			myCharts.value.setOption( {
+				animation: false,
+				radar: {
+					shape: "circle",
+					center: [ "50%", "50%" ],
+					radius: "65%",
+					indicator: props.data.list.map( d => {
+						return {
+							name: d.label,
+							max: total
+						}
+					} ),
+					axisName: {
+						color: props.color.text,
+						fontSize: 14,
+						fontFamily: "GenshinUsedFont, monospace",
+						formatter: value => {
+							const format = [ "武器等级", "天赋升级" ];
+							if ( format.includes( value ) ) {
+								const arr = value.split( "" );
+								arr.splice( 2, 0, "\n" );
+								return arr.join( "" );
+							}
+							return value;
+						},
+					},
+					axisLine: {
+						show: true
 					}
-				} ),
-				axisName: {
-					color: props.color.text,
-					fontSize: 14,
-					fontFamily: "GenshinUsedFont, monospace",
-					formatter: value => {
-						const format = [ "武器等级", "天赋升级" ];
-						if ( format.includes( value ) ) {
-							const arr = value.split( "" );
-							arr.splice( 2, 0, "\n" );
-							return arr.join( "" );
-						}
-						return value;
-					},
 				},
-				axisLine: {
-					show: true
-				}
-			},
-			series: [
-				{
-					type: 'radar',
-					areaStyle: {
-						color: props.color.graphic,
-						opacity: 0.4
-					},
-					itemStyle: {
-						opacity: 0
-					},
-					lineStyle: {
-						color: props.color.graphic,
-						opacity: 0.6
-					},
-					data: [
-						{
-							value: props.data.list.map( d => d.percentage * total ),
-						}
-					]
-				}
-			]
+				series: [
+					{
+						type: "radar",
+						areaStyle: {
+							color: props.color.graphic,
+							opacity: 0.4
+						},
+						itemStyle: {
+							opacity: 0
+						},
+						lineStyle: {
+							color: props.color.graphic,
+							opacity: 0.6
+						},
+						data: [
+							{
+								value: props.data.list.map( d => d.percentage * total ),
+							}
+						]
+					}
+				]
+			} );
+		}
+
+		watch( () => props.data, () => {
+			initEcharts();
 		} )
-		
-		/* 字体加载完毕后刷新e-chart */
-		const timer = ref( null );
-		
+
 		onMounted( () => {
-			timer.value = setInterval( () => {
-				if ( document.readyState === "complete" ) {
-					chartKey.value++;
-					window.clearInterval( timer.value );
-					timer.value = null;
-				}
-			}, 20 )
+			if ( !echartsRef.value ) {
+				return;
+			}
+			myCharts.value = echarts.init( echartsRef.value );
+			initEcharts();
+
+			if ( document.readyState !== "complete" ) {
+				window.addEventListener( "load", () => initEcharts );
+			}
+
+			window.addEventListener( "resize", () => {
+				myCharts.value?.resize();
+			} );
 		} );
-		
+
 		return {
 			score,
-			chartKey,
-			chartOption
+			echartsRef
 		}
 	}
 } )
--- views/ledger/components/data-chart.js
+++ views/ledger/components/data-chart.js
@@ -1,17 +1,13 @@
 const template = `<div class="data-chart">
-	<VChart class="chart" :option="chartOption" :key="chartKey"></VChart>
+	<div class="chart" ref="echartsRef"></div>
 </div>`;
 
-import { defineComponent, ref, onMounted, computed } from "vue";
-import "echarts";
-import VChart from "vue-echarts";
+import { defineComponent, ref, onMounted, watch } from "vue";
+import * as echarts from "echarts";
 
 export default defineComponent( {
 	name: "DataChart",
 	template,
-	components: {
-		VChart
-	},
 	props: {
 		data: {
 			type: Array,
@@ -19,61 +15,73 @@ export default defineComponent( {
 		}
 	},
 	setup( props ) {
-		const chartKey = ref( 0 );
-		
-		const chartOption = computed(() => ({
-			animation: false,
-			legend: {
-				orient: "vertical",
-				left: 380,
-				top: "center",
-				align: "left",
-				data: props.data.map( d => `${ d.action } ${ d.percent }%` ),
-				icon: "rect",
-				itemWidth: 15,
-				itemHeight: 15,
+		const myCharts = ref( null );
+		const echartsRef = ref( null );
+
+		function initEcharts() {
+			if ( !myCharts.value ) {
+				return;
+			}
+			myCharts.value.setOption( {
+				animation: false,
+				legend: {
+					orient: "vertical",
+					left: 380,
+					top: "center",
+					align: "left",
+					data: props.data.map( d => `${ d.action } ${ d.percent }%` ),
+					icon: "rect",
+					itemWidth: 15,
+					itemHeight: 15,
+					textStyle: {
+						color: "#fff",
+					}
+				},
 				textStyle: {
 					color: "#fff",
-				}
-			},
-			textStyle: {
-				color: "#fff",
-				fontFamily: "GenshinUsedFont, monospace"
-			},
-			series: [
-				{
-					type: "pie",
-					right: 200,
-					data: props.data.map( d => {
-						return {
-							name: `${ d.action } ${ d.percent }%`,
-							value: d.num
+					fontFamily: "GenshinUsedFont, monospace"
+				},
+				series: [
+					{
+						type: "pie",
+						right: 200,
+						data: props.data.map( d => {
+							return {
+								name: `${ d.action } ${ d.percent }%`,
+								value: d.num
+							}
+						} ),
+						radius: [ "60%", "75%" ],
+						label: {
+							show: false
 						}
-					} ),
-					radius: [ "60%", "75%" ],
-					label: {
-						show: false
 					}
-				}
-			]
-		}) )
+				]
+			} );
+		}
 		
-		/* 字体加载完毕后刷新e-chart */
-		const timer = ref( null );
+		watch( () => props.data, () => {
+			initEcharts();
+		} )
 		
 		onMounted( () => {
-			timer.value = setInterval( () => {
-				if ( document.readyState === "complete" ) {
-					chartKey.value++;
-					window.clearInterval( timer.value );
-					timer.value = null;
-				}
-			}, 20 )
+			if ( !echartsRef.value ) {
+				return;
+			}
+			myCharts.value = echarts.init( echartsRef.value );
+			initEcharts();
+
+			if ( document.readyState !== "complete" ) {
+				window.addEventListener( "load", () => initEcharts );
+			}
+
+			window.addEventListener( "resize", () => {
+				myCharts.value?.resize();
+			} );
 		} );
-		
+
 		return {
-			chartKey,
-			chartOption
+			echartsRef
 		}
 	}
 } )
