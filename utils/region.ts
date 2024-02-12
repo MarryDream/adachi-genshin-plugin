77ae48627ff2d3a5106cd938b8401a0e381aa05b
MarryDream <2037177696@qq.com>
2024-02-12 18:13:17 +0800

    feat: support for 3xxx uid

--- utils/region.ts
+++ utils/region.ts
@@ -1,8 +1,7 @@
 export function getRegion( first: string ): string {
-	switch ( first ) {
-		case "1": return "cn_gf01";
-		case "2": return "cn_gf01";
-		case "5": return "cn_qd01";
-		default:  return "unknown";
-	}
+	if ( [ "1", "2", "3" ].includes( first ) ) return "cn_gf01";
+	
+	if ( first === "5" ) return "cn_qd01";
+	
+	return "unknown";
 }
